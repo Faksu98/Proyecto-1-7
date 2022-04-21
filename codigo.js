4ef900e3a56498a19b92369f23bc61e3f3402124
faksu <dk>
2022-04-21 11:22:49 -0300

    Variables

---
+++ codigo.js
@@ -0,0 +1,37 @@
+var numeroFavorito = 14;
+
+var anioActual = 2022;
+
+var nombreMascotas = "umma y zando";
+
+var nombreLibro = "el archivo de las tormentas";
+
+var haceFrio = true;
+
+var trabjarFeriados = false;
+
+var valorLibros = 8097.90;
+
+var valorSilla = 29999.99;
+
+
+//console.log ( numeroFavorito );
+//console.log ( anioActual );
+//console.log ( nombreMascotas );
+//console.log ( nombreLibro );
+//console.log ( haceFrio );
+//console.log ( trabjarFeriados );
+//console.log ( valorLibros );
+//console.log ( valorSilla );
+
+
+document.write (`
+  <h4> Mi numero favorito es ${ numeroFavorito }  </h4>
+  <h4> Nos encontramos en el año ${ anioActual }  </h4>
+  <h4> Mis perros se llaman ${ nombreMascotas }  </h4>
+  <h4> Mi saga de libros favorita es ${ nombreLibro }  </h4>
+  <h4> ¿Hoy hace frio? ${ haceFrio }  </h4>
+  <h4> ¿Los feriados se labura? ${ trabjarFeriados }  </h4>
+  <h4> Precio del camino de los reyes ${ valorLibros }  </h4>
+  <h4> Precio de silla gamer ${ valorSilla }  </h4>
+`)
